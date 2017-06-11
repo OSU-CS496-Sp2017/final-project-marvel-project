a6b8c41a95bba5782b5860c280883fbe94276262
j1298666 <chiens>
2017-06-10 20:09:45 -0700

    buttons

--- MarvelDatabase/app/src/main/java/com/example/btanner/marveldatabase/MainActivity.java
+++ MarvelDatabase/app/src/main/java/com/example/btanner/marveldatabase/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.btanner.marveldatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button characterButton = (Button)findViewById(R.id.btn_characters);
+        Button commicButton = (Button)findViewById(R.id.btn_comics);
+        Button creatorButton = (Button)findViewById(R.id.btn_creators);
+        Button eventButton = (Button)findViewById(R.id.btn_events);
+        Button seriesButton = (Button)findViewById(R.id.btn_series);
+        Button storiesButton = (Button)findViewById(R.id.btn_stories);
+
     }
 
     public void doCharacters(View v) {
